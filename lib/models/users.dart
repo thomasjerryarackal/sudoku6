7c26c47c7a920b930aeb7852fb7ee0fc8c627ffb
computer <thomasjerryarackal@gmail.com>
2021-01-30 18:27:03 +0530

    grwgwgw

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -4,3 +4,13 @@ class UserK {
   UserK({ this.uid});
 
 }
+
+
+class UserData {
+  final String uid;
+  final String name;
+  final String sugars;
+  final  int strength;
+
+  UserData({this.uid,this.name,this.sugars,this.strength});
+}
