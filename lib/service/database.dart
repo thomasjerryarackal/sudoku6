831fdfdbd3062268077fdbb97a62c1b6fc132216
computer <thomasjerryarackal@gmail.com>
2021-01-30 18:27:29 +0530

    eee

--- lib/service/database.dart
+++ lib/service/database.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sudoku/models/sudoku.dart';
+import 'package:sudoku/models/users.dart';
 
 class DatabaseService {
 
@@ -17,4 +19,42 @@ class DatabaseService {
 
     });
   }
+
+
+  // sudoku list from the snapshot
+  List<Sudoku> _sudokuListFromSnapshot(QuerySnapshot snapshot){
+     return snapshot.docs.map((doc) {
+      return Sudoku(
+        name:doc.data()["name"] ?? '',
+        strenght: doc.data()["strenght"] ?? 0,
+        sugar:doc.data()["sugar"] ?? '0',
+
+      );
+     }).toList();
+  }
+
+  //user data from snapshots
+  UserData _userDataFromSnapshot(DocumentSnapshot documentSnapshot) {
+    return UserData(
+        uid: uid,
+        name: documentSnapshot.data()["name"],
+        sugars: documentSnapshot.data()["sugars"],
+        strength: documentSnapshot.data()["strength"],
+    );
+  }
+
+
+  // get sudoku stream
+ Stream<List<Sudoku>> get sudokus{
+    return sudokuCollection.snapshots()
+    .map(_sudokuListFromSnapshot);
+ }
+
+ // get database-user document stream
+ Stream<DocumentSnapshot> get userData  {
+    return sudokuCollection.doc(uid).snapshots()
+        .map(_userDataFromSnapshot);
+ }
+
+
 }
